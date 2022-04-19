eaa6ef29b6d1b2f6f5eda3a826573324549f5c6e
AgustinRossi123 <101335570+AgustinRossi123@users.noreply.github.com>
2022-04-19 12:01:20 -0300

    cambios_1

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayorDeEdad : MonoBehaviour
+{
+    public int edad;
+    public Text mitexto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edad >= 18)
+        {
+            mitexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else if(edad <= 18)
+        {
+            mitexto.text = "No es mayor de edad";
+            Debug.Log("Es menor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
