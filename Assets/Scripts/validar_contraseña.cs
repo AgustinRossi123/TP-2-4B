9c1ac4fa4097de95e11426206ecd4edabba06a62
AgustinRossi123 <101335570+AgustinRossi123@users.noreply.github.com>
2022-04-20 13:35:37 -0300

    cambio_2

--- Assets/Scripts/validar_contraseña.cs
+++ Assets/Scripts/validar_contraseña.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class validar_contraseña : MonoBehaviour
 {
+    string contraseniacorrecta;
+    string contraseniausuario;
+    public Text ingresousuario;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraseniacorrecta = "12345";    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ValidarContrasenia()
+    {
+        contraseniausuario = ingresousuario.text;
+        if (contraseniausuario==contraseniacorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 }
